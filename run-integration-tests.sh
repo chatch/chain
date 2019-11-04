73479b9d5d610a4cc7cd3e68141813523f5c3bf3
Calvin Lau <calvinlauco@gmail.com>
2019-11-04 14:57:03 +0800

    Problem: (CRO-533) Drone CI cannot run tests in parallel and tear down properly
    Solution: Use branch name as docker-compose prefix and teardown properly

--- run-integration-tests.sh
+++ run-integration-tests.sh
@@ -7,19 +7,21 @@ rustup default nightly-2019-08-01-x86_64-unknown-linux-gnu
 cd integration-tests
 ./prepare.sh || exit 1
 . ./env.sh
-docker-compose down || exit 1
+docker-compose -p "${DOCKER_COMPOSE_PREFIX}" down || (docker ps; exit 1)
 
-docker-compose up -d || exit 1
+# Find ports for Docker
+export TENDERMINT_RPC_PORT=$(./find-free-port.sh)
+export CLIENT_RPC_PORT=$(./find-free-port.sh)
+export TENDERMINT_ZEROFEE_RPC_PORT=$(./find-free-port.sh)
+export CLIENT_RPC_ZEROFEE_PORT=$(./find-free-port.sh)
+docker-compose -p "${DOCKER_COMPOSE_PREFIX}" up -d || (docker ps; exit 1)
 
-./wait-for-setup.sh || (docker ps && docker-compose logs -t --tail="all" && exit 1)
+./wait-for-setup.sh || (docker ps; docker-compose -p "${DOCKER_COMPOSE_PREFIX}" logs -t --tail="all"; exit 1)
 
 # Preliminary tests
-./run-test.sh || (docker ps && docker-compose logs -t --tail="all" && exit 1)
+./run-test.sh || (docker ps; docker-compose -p "${DOCKER_COMPOSE_PREFIX}" logs -t --tail="all"; exit 1)
 
 # Integration tests
 cd client-rpc
 npm install
-npm run test || (docker ps && docker-compose logs -t --tail="all" && exit 1)
-
-# Teardown
-docker-compose down
+npm run test || (docker ps; docker-compose -p "${DOCKER_COMPOSE_PREFIX}" logs -t --tail="all"; exit 1)
